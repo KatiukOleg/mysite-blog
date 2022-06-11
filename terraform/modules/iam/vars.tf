6012c5d7c484fa99ea92bc3311ad85aa807dfbb9
Oleh Katiuk <okati@softserveinc.com>
2022-06-12 00:47:06 +0300

    add terraform folder, iam module

---
+++ terraform/modules/iam/vars.tf
@@ -0,0 +1,11 @@
+variable "Region" {
+  description = "AWS region"
+}
+
+variable "Environment" {
+  description = "Deployment Environment"
+}
+
+variable "Project" {
+  description = "Project Name"
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    organization = "KatiukOlegOrg"
+    workspaces {
+      name = "mysite-blog"
+    }
+  }
+  
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.8.0"
+    }
+  }
+
+  required_version = ">= 1.1.7"
+}
+
+provider "aws" {
+  #profile = var.AWS_PROFILE
+  region = var.AWS_REGION
+}
