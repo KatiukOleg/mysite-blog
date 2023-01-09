9a1484903a3746d84d508c9c93437025b5ecbbc9
Oleh Katiuk <okati@softserveinc.com>
2023-01-09 20:01:51 +0200

    addition conditions for envs

--- mysite/mysite/settings/__init__.py
+++ mysite/mysite/settings/__init__.py
@@ -5,3 +5,7 @@ if os.getenv('ENV') == 'DEV':
     from .dev import *
 elif os.getenv('ENV') == 'LOCAL':
     from .local import *
+elif os.getenv('ENV') == 'STAGE':
+    from .local import *
+elif os.getenv('ENV') == 'PROD':
+    from .local import *
